3b4b1a56c6bc957d5837a3446e138893f328f524
Erick Bores <e.bores.i@outlook.com>
2026-03-17 10:07:36 -0600

    fix: use string literal instead of String.raw in middleware matcher

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -74,6 +74,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * - public files (public folder)
      */
-    String.raw`/((?!_next/static|_next/image|favicon.ico|.*\..*|api).*)`,
+    '/((?!_next/static|_next/image|favicon.ico|.*\\..*|api).*)',
   ],
 };
